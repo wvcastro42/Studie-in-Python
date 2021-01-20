5c380f9cfc3b33820eecdb673c22bee738c35beb
Weverton Castro <wvcastro42@gmail.com>
2021-01-20 02:52:54 -0300

    While pt.2

--- PythonParaZumbis/while.py
+++ PythonParaZumbis/while.py
@@ -4,10 +4,78 @@
 #     print(x)
 #     x += 1
 
-'''Imprimir os número pares'''
-fim = int(input('Digite um número: '))
-x = 0
-while x <= fim:
-    if x % 2 == 0:
-        print(x)
-    x += 1
+# '''Imprimir os número pares'''
+# fim = int(input('Digite um número: '))
+# x = 0
+# while x <= fim:
+#     if x % 2 == 0:
+#         print(x)
+#     x += 1
+
+# ''' Imprimir e somar os 10 números digitados'''
+# n = 1
+# soma = 0
+# while n <= 10:
+#     x = int(input(f'{n} número: '))
+#     soma += x
+#     n += 1
+# print(f'Soma: {soma}')
+
+# ''' Imprimir e tirar a média dos 10 números digitados'''
+# n = 1
+# media = 0
+# while n <= 10:
+#     x = int(input(f'{n} número: '))
+#     media += x
+#     n += 1
+# print(f'Média: {media/10}')
+
+#
+# ''' Fatorial de um número lido '''
+#
+# n = int(input('Digite um número: '))
+# i = 1
+# fat = 1
+# while i <= n:
+#     fat = fat * i
+#     i += 1
+# print(f'fat({n}) = {fat}')
+
+''' Média até digitar 0 '''
+soma = 0
+n = 0
+while True:
+    x = float(input('n (zero sai): '))
+    if x == 0:
+        break
+    else:
+        soma = soma + x
+        n += 1
+print(f'Média: {soma/n:.1f}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#.
