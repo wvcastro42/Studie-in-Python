6339aa17bdb19377791c1927ec7441313222fa4c
Weverton Castro <wvcastro42@gmail.com>
2021-05-26 15:25:49 -0300

    a

--- URI/ex1045.py
+++ URI/ex1045.py
@@ -1,5 +1,6 @@
 """
-Leia 3 valores de ponto flutuante A, B e C e ordene-os em ordem decrescente, de modo que o lado A representa o maior dos 3 lados. A seguir, determine o tipo de triângulo que estes três lados formam, com base nos seguintes casos, sempre escrevendo uma mensagem adequada:
+Leia 3 valores de ponto flutuante A, B e C e ordene-os em ordem decrescente, de modo que o lado A representa o maior dos 3 lados. A seguir, determine o tipo de triângulo que estes três lados formam, com base nos seguintes casos,
+sempre escrevendo uma mensagem adequada:
 se A ≥ B+C, apresente a mensagem: NAO FORMA TRIANGULO
 se A2 = B2 + C2, apresente a mensagem: TRIANGULO RETANGULO
 se A2 > B2 + C2, apresente a mensagem: TRIANGULO OBTUSANGULO
@@ -12,3 +13,10 @@ A entrada contem três valores de ponto flutuante de dupla precisão A (0 < A) ,
 Saída
 Imprima todas as classificações do triângulo especificado na entrada.
 """
+
+
+a = [1,2,6,4,7]
+>>> a.sort(reverse=True)
+>>> a
+[7, 6, 4, 2, 1]
+>>>
