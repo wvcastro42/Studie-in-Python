7ece118c726ff55fbf5a04d139823a60d02f7702
Weverton Castro <wvcastro42@gmail.com>
2021-05-27 17:07:05 -0300

    URI

---
+++ URI/ex1046.py
@@ -0,0 +1,17 @@
+"""
+Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
+
+Entrada
+A entrada contém dois valores inteiros representando a hora de início e a hora de fim do jogo.
+
+Saída
+Apresente a duração do jogo conforme exemplo abaixo.
+"""
+inicio, fim = (map(int, input().split()))
+
+hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+
+if inicio == fim:
+    print('O JOGO DUROU 24 HORA(S)')
+else:
+    print(f'O JOGO DUROU {hours[inicio:].index(fim)} HORA(S)')
