934e9c1f16fb52cfd8f2f6526365bef73f90bed0
Weverton Castro <wvcastro42@gmail.com>
2021-06-05 16:47:36 -0300

    CheckIO

---
+++ CheckIO/Home/right_to_left.py
@@ -0,0 +1,33 @@
+# return (",".join(phrases)).replace("right","left")
+
+
+def left_join(phrases: tuple) -> str:
+
+    phrase = ''
+    for idx, element in enumerate(phrases):
+        if 'right' in element:
+            if len(phrases) == (idx+1):
+                phrase = phrase + element.replace('right', 'left')
+            else:
+                phrase = phrase + element.replace('right', 'left') + ','
+
+        else:
+            if len(phrases) == (idx+1):
+                phrase = phrase + element
+            else:
+                phrase = phrase + element + ','
+
+    return phrase
+
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(left_join(("left", "right", "left", "stop")))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert left_join(("left", "right", "left", "stop")) == "left,left,left,stop", "All to left"
+    assert left_join(("bright aright", "ok")) == "bleft aleft,ok", "Bright Left"
+    assert left_join(("brightness wright",)) == "bleftness wleft", "One phrase"
+    assert left_join(("enough", "jokes")) == "enough,jokes", "Nothing to replace"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
