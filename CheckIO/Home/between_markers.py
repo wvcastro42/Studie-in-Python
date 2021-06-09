aff837971225c2888dd3d23e7036c0edeabfe434
Weverton Castro <wvcastro42@gmail.com>
2021-06-09 15:44:10 -0300

    CheckIO - Between markers

--- CheckIO/Home/between_markers.py
+++ CheckIO/Home/between_markers.py
@@ -1,38 +1,40 @@
 def between_markers(text: str, begin: str, end: str) -> str:
-    # print(begin in text)
+
+    if begin not in text and end in text:
+        stop = (text.find(end))
+        return text[:stop]
+
+    if end not in text and begin in text:
+        start = (text.find(begin)) + (len(begin))
+        return text[start:]
+
     if begin not in text and end not in text:
         return text
 
-    elif text.find(end) < text.find(begin):
+    if (text.find(end)) < (text.find(begin)):
         return ''
 
-    elif begin not in text:
-        return text[:text.find(end)]
-
-    elif end not in text:
-            print('oiSW')
-            start = (text.find(begin)) + (len(begin))
-            return text[start:]
-
-    else:
-        start = (text.find(begin)) + (len(begin))
-        return text[start:text.find(end)]
-
-print(between_markers('No [b]hi', '[b]', '[/b]'))
+    start = text.find(begin) + len(begin)
+    stop = text.find(end)
+    return text[start:stop]
+#OR
+# start = text.find(begin) + len(begin) if begin in text else None
+# stop = text.find(end) if end in text else None
+# return text[start:stop]
 
 
-# if __name__ == '__main__':
-#     print('Example:')
-#     print(between_markers('What is >apple<', '>', '<'))
-#     # These "asserts" are used for self-checking and not for testing
-#     assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
-#     assert between_markers("<head><title>My new site</title></head>",
-#                            "<title>", "</title>") == "My new site", "HTML"
-#     assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
-#     assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
-#     assert between_markers('No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
-#     assert between_markers('No <hi>', '>', '<') == '', 'Wrong direction'
-#     print('Wow, you are doing pretty good. Time to check it!')
+if __name__ == '__main__':
+    print('Example:')
+    print(between_markers('What is >apple<', '>', '<'))
+    # These "asserts" are used for self-checking and not for testing
+    assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
+    assert between_markers("<head><title>My new site</title></head>",
+                           "<title>", "</title>") == "My new site", "HTML"
+    assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
+    assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
+    assert between_markers('No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
+    assert between_markers('No <hi>', '>', '<') == '', 'Wrong direction'
+    print('Wow, you are doing pretty good. Time to check it!')
 
 """
 You are given a string and two markers (the initial and final). You have to find a substring enclosed between these two markers. But there are a few important conditions:
