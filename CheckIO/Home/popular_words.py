bc4c238f677596b9967fec679f8fc783d8182881
Weverton Castro <wvcastro42@gmail.com>
2021-06-12 14:51:37 -0300

    popular_words.py

--- CheckIO/Home/popular_words.py
+++ CheckIO/Home/popular_words.py
@@ -1,7 +1,29 @@
 def popular_words(text: str, words: list) -> dict:
 
+    lower_count = text.lower().split().count
+    return {word: lower_count(word) for word in words}
 
+    # text = text.lower().split()
+    # dict_words = {}
+    #
+    # for word in words:
+    #     count = 0
+    #     if word in text:
+    #         for w in text:
+    #             if word == w:
+    #                 count += 1
+    #         dict_words[word] = count
+    #     else:
+    #         dict_words[word] = count
+    #
+    # return dict_words
 
+# print(popular_words('''
+# When I was One
+# I had just begun
+# When I was Two
+# I was nearly new
+# ''', ['i', 'was', 'three', 'near']))
 
 if __name__ == '__main__':
     print("Example:")
