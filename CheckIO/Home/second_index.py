f3fe24703260ef324cefba3da9792bec919a3b6f
Weverton Castro <wvcastro42@gmail.com>
2021-06-16 16:04:12 -0300

    second index - CheckIO

---
+++ CheckIO/Home/second_index.py
@@ -0,0 +1,19 @@
+def second_index(text: str, symbol: str) -> [int, None]:
+
+    if text.count(symbol) >= 2:
+        return text[(text.index(symbol)+1):].index(symbol) + text.index(symbol) + 1 if True else None
+    return None
+
+# print (second_index("find the river", "e"))
+
+if __name__ == '__main__':
+    print('Example:')
+    print(second_index("sims", "s"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert second_index("sims", "s") == 3, "First"
+    assert second_index("find the river", "e") == 12, "Second"
+    assert second_index("hi", " ") is None, "Third"
+    assert second_index("hi mayor", " ") is None, "Fourth"
+    assert second_index("hi mr Mayor", " ") == 5, "Fifth"
+    print('You are awesome! All tests are done! Go Check it!')
