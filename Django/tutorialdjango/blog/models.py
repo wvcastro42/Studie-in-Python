271a40203d708d1027c3163293050e2bead816a0
Weverton Castro <wvcastro42@gmail.com>
2021-07-18 03:23:51 -0300

    Django Blog - Fabio Ruicci

--- Django/tutorialdjango/blog/models.py
+++ Django/tutorialdjango/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 #cria a Tabela post, do APP Blob, com os seguintes campos:
 class Post(models.Model): 
@@ -10,5 +11,11 @@ class Post(models.Model):
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
 
+    class Meta:
+        ordering = ("-created",)
+
     def __str__(self) -> str: # para aparecer o título do post quando exibir a lista de posts
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={'slug': self.slug})
