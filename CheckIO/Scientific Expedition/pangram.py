2bfc380ee7bbb6410ca2ed2ffaafa9acc7de5fa9
Weverton Castro <wvcastro42@gmail.com>
2021-08-16 13:01:38 -0300

    CheckIO - Pangram

---
+++ CheckIO/Scientific Expedition/pangram.py
@@ -0,0 +1,20 @@
+def check_pangram(text):
+
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    text_set = set(text.lower())
+    text_list = list(sorted(text_set))
+    result = []
+
+    for i in text_list:
+        if i.isalpha():
+            result.append(i)
+
+    return alphabet == result
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_pangram("The quick brown fox jumps over the lazy dog."), "brown fox"
+    assert not check_pangram("ABCDEF"), "ABC"
+    assert check_pangram("Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!"), "Bored?"
+    print('If it is done - it is Done. Go Check is NOW!')
