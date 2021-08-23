7482aa07c196cb212a3bcca53bc7a34413590266
Weverton Castro <wvcastro42@gmail.com>
2021-08-23 17:29:28 -0300

    URI - 1067

---
+++ URI/ex1067.py
@@ -0,0 +1,9 @@
+[print(i) for i in range(int(input())+1) if i%2 == 1]
+
+# num = int(input())
+# for i in range(num):
+#     if i%2 == 1: print(i)
+
+
+# for i in range(int(input())):
+#     if i%2 == 1: print(i)
