bec9a7e5f239b55edee9d3fc55408f1209962520
Weverton Castro <wvcastro42@gmail.com>
2021-08-31 19:12:32 -0300

    CheckIO - Remove Brackets

--- CheckIO/Scientific Expedition/remove_brackets.py
+++ CheckIO/Scientific Expedition/remove_brackets.py
@@ -1,47 +1,36 @@
-from pythonds.basic import Stack
+import itertools
 
-def parChecker(symbolString):
-    s = Stack()
-    balanced = True
-    index = 0
-    while index < len(symbolString) and balanced:
-        symbol = symbolString[index]
-        if symbol == "(":
-            s.push(symbol)
+
+def is_balanced(a):
+    while True:
+        if '()' in a:
+            a = a.replace('()', '')
+        elif '[]' in a:
+            a = a.replace('[]', '')
+        elif '{}' in a:
+            a = a.replace('{}', '')
         else:
-            if s.isEmpty():
-                balanced = False
-            else:
-                s.pop()
-
-        index = index + 1
-
-    if balanced and s.isEmpty():
-        print (s)
-        return s
-    else:
-        print (s)
-        return s
-
-print(parChecker('((()))'))
-print(parChecker('(()'))
-
-# def remove_brackets(line: str) -> str:
+            return not a
 
 
-# print(remove_brackets('[[(}]]'))
+def remove_brackets(a):
+    for i in range(len(a)+1):
+        for c in itertools.combinations(range(len(a)), i):
+            s = ''.join(v for j, v in enumerate(a) if j not in c)
+            if is_balanced(s):
+                return s
 
-# if __name__ == '__main__':
-#     print("Example:")
-#     print(remove_brackets('(()()'))
+if __name__ == '__main__':
+    print("Example:")
+    print(remove_brackets('(()()'))
 
     # These "asserts" are used for self-checking and not for an auto-testing
-    # assert remove_brackets('(()()') == '()()'
-    # assert remove_brackets('[][[[') == '[]'
-    # assert remove_brackets('[[(}]]') == '[[]]'
-    # assert remove_brackets('[[{}()]]') == '[[{}()]]'
-    # assert remove_brackets('[[[[[[') == ''
-    # assert remove_brackets('[[[[}') == ''
-    # assert remove_brackets('') == ''
-    # assert remove_brackets('[(])') == '()'
-    # print("Coding complete? Click 'Check' to earn cool rewards!")
+    assert remove_brackets('(()()') == '()()'
+    assert remove_brackets('[][[[') == '[]'
+    assert remove_brackets('[[(}]]') == '[[]]'
+    assert remove_brackets('[[{}()]]') == '[[{}()]]'
+    assert remove_brackets('[[[[[[') == ''
+    assert remove_brackets('[[[[}') == ''
+    assert remove_brackets('') == ''
+    assert remove_brackets('[(])') == '()'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
